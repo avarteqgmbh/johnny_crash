761ea7cf268e6824c770600eff25ed6377cc3c39
Julian Fischer <fischer@enterprise-rails.de>
2013-02-13 16:43:43 +0100

    Readme started; Started to fromat Errors#show

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -7,14 +7,24 @@ class ErrorsController < ApplicationController
   end
   
   def show
-    @error = Error.find(params[:id])
+    @error  = Error.find(params[:id])
+    @json   = JSON.parse(@error.payload)
   end
   
   def create
+    params.delete("action")
+    params.delete("controller")
     Error.create(payload: params.to_json)
     render :nothing => true
   end
   
   def update
+  end
+  
+  def destroy
+    @error = Error.find(params[:id])
+    @error.destroy
+    flash[:notice] = "Error successfully deleted."
+    redirect_to errors_path
   end
 end
